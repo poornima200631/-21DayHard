1571bbe1b83228c8cc926eb66a188d8424f47883
poornima200631 <poornimatewatia48@gmail.com>
2025-03-21 07:42:10 +0530

    push day1 code

--- BinarySearch:Day1.cpp
+++ BinarySearch:Day1.cpp
@@ -48,3 +48,45 @@ class Solution {
 
 // TC:O(n);
 // SC:1;
+
+// Given an unsorted array arr[] of integers and an integer x, find the floor and ceiling of x in arr[].
+
+// Floor of x is the largest element which is smaller than or equal to x. Floor of x doesn’t exist if x is smaller than smallest element of arr[].
+// Ceil of x is the smallest element which is greater than or equal to x. Ceil of x doesn’t exist if x is greater than greatest element of arr[].
+
+// Return an array of integers denoting the [floor, ceil]. Return -1 for floor or ceiling if the floor or ceiling is not present.
+
+
+class Solution {
+  public:
+    vector<int> getFloorAndCeil(int x, vector<int> &arr) {
+        int n= arr.size();
+        int Floor=INT_MIN;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>=Floor&&arr[i]<=x){
+            Floor=arr[i];
+            count++;
+            }
+        }
+        if(count==0)
+        Floor= -1;
+        int count1=0;
+        int Ceil=INT_MAX;
+         for(int i=0;i<n;i++){
+            if(arr[i]<=Ceil&&arr[i]>=x){
+            Ceil=arr[i];
+            count1++;
+            }
+        }
+        if(count1==0)
+        Ceil= -1;
+        vector<int>v;
+        v.push_back(Floor);
+         v.push_back(Ceil);
+         return v;
+    }
+};
+
+// TC:O(n);
+// SC:1;
