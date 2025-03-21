3d7d4ed56c5c11ef2d1302f84815c5d96fa24a45
poornima200631 <poornimatewatia48@gmail.com>
2025-03-21 06:54:51 +0530

    push day1 code

--- BinarySearch:Day1.cpp
+++ BinarySearch:Day1.cpp
@@ -25,3 +25,26 @@ public:
     return -1;
     }
 };
+
+// TC:O(Log n);
+// SC:1;
+
+// Given a sorted array arr[] and an integer x, find the index (0-based) of the largest element in arr[] that is less than or equal to x. This element is called the floor of x. If such an element does not exist, return -1.
+
+// Note: In case of multiple occurrences of ceil of x, return the index of the last occurrence.
+
+class Solution {
+  public:
+
+    int findFloor(vector<int>& arr, int x) {
+       int n=arr.size();
+       for(int i=n-1;i>=0;i--){
+           if(arr[i]<=x)
+           return i;
+       }
+       return -1;
+    }
+};
+
+// TC:O(n);
+// SC:1;
